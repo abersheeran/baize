f9a4613199b3e9e2745facfa2e3d357de1940fc0
Aber <me@abersheeran.com>
2021-08-02 14:49:18 +0800

    Delete mypyc.py

--- baize/mypyc.py
+++
@@ -1,6 +0,0 @@
-try:
-    from mypy_extensions import mypyc_attr
-except ImportError:
-    allow_interpreted_subclasses = lambda x: x
-else:
-    allow_interpreted_subclasses = mypyc_attr(allow_interpreted_subclasses=True)
