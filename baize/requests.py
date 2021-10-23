463397c76428b2abc5ef0ae2e35a9bd943e49876
abersheeran <me@abersheeran.com>
2021-10-23 22:42:31 +0800

    Add type hint for mypyc

--- baize/requests.py
+++ baize/requests.py
@@ -30,6 +30,8 @@ class MoreInfoFromHeaderMixin:
     def headers(self) -> Headers:
         raise NotImplementedError
 
+    _accepted_types: List[MediaType]
+
     @property
     def accepted_types(self) -> List[MediaType]:
         """
@@ -51,6 +53,8 @@ class MoreInfoFromHeaderMixin:
             accepted_type.match(media_type) for accepted_type in self.accepted_types
         )
 
+    _content_type: ContentType
+
     @property
     def content_type(self) -> ContentType:
         """
@@ -59,6 +63,8 @@ class MoreInfoFromHeaderMixin:
         if not hasattr(self, "_content_type"):
             self._content_type = ContentType(self.headers.get("content-type", ""))
         return self._content_type
+
+    _content_length: Optional[int]
 
     @property
     def content_length(self) -> Optional[int]:
@@ -78,6 +84,8 @@ class MoreInfoFromHeaderMixin:
                     except (ValueError, TypeError):
                         self._content_length = None
         return self._content_length
+
+    _cookies: Dict[str, str]
 
     @property
     def cookies(self) -> Dict[str, str]:
@@ -109,6 +117,8 @@ class MoreInfoFromHeaderMixin:
             self._cookies = cookies
         return self._cookies
 
+    _date: Optional[datetime]
+
     @property
     def date(self) -> Optional[datetime]:
         """
@@ -131,6 +141,8 @@ class MoreInfoFromHeaderMixin:
                     else:
                         self._date = date
         return self._date
+
+    _referrer: Optional[URL]
 
     @property
     def referrer(self) -> Optional[URL]:
