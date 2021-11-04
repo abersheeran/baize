93cefe09de3d2b9f28b2838e36723f2bcc26ceef
abersheeran <me@abersheeran.com>
2021-11-04 22:53:08 +0800

    Add complete examples

---
+++ examples/asynchronous.py
@@ -0,0 +1,46 @@
+import time
+from typing import Awaitable, Callable
+from baize.asgi import (
+    middleware,
+    request_response,
+    Router,
+    Request,
+    Response,
+    PlainTextResponse,
+)
+
+
+@middleware
+async def timer(
+    request: Request, next_call: Callable[[Request], Awaitable[Response]]
+) -> Response:
+    start_time = time.time()
+    response = await next_call(request)
+    end_time = time.time()
+    response.headers["x-time"] = str(round((end_time - start_time) * 1000))
+    return response
+
+
+@request_response
+@timer
+async def sayhi(request: Request) -> Response:
+    return PlainTextResponse("hi, " + request.path_params["name"])
+
+
+@request_response
+@timer
+async def echo(request: Request) -> Response:
+    return PlainTextResponse(await request.body)
+
+
+application = Router(
+    ("/", PlainTextResponse("homepage")),
+    ("/echo", echo),
+    ("/sayhi/{name}", sayhi),
+)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(application, interface="asgi3", port=8000)
