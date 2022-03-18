231c0108c3461947a845d5b65ab2771250a7c8a3
abersheeran <me@abersheeran.com>
2022-03-18 06:58:22 +0000

    Copy parse_header from stdlib cgi

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,6 @@
-from baize.utils import cached_property
+import pytest
+
+from baize.utils import cached_property, parse_header
 
 
 def test_cached_property():
@@ -9,3 +11,28 @@ def test_cached_property():
 
     assert T.li.__name__ == "li"
     assert not callable(T.li)
+
+
+@pytest.mark.parametrize(
+    "line,result",
+    [
+        (
+            "application/json",
+            ("application/json", {}),
+        ),
+        (
+            "text/html; charset=utf-8",
+            ("text/html", {"charset": "utf-8"}),
+        ),
+        (
+            'form-data; name="lname"; filename="bob"',
+            ("form-data", {"name": "lname", "filename": "bob"}),
+        ),
+        (
+            'value; name="baize;tests"',
+            ("value", {"name": "baize;tests"}),
+        ),
+    ],
+)
+def test_parse_header(line, result):
+    assert parse_header(line) == result
