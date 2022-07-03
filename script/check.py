a9914a2da36eab180802f742d3d899722ceb3890
abersheeran <me@abersheeran.com>
2022-07-03 11:52:35 +0000

    Fix mypy check contextvars

--- script/check.py
+++ script/check.py
@@ -4,4 +4,4 @@ source_dirs = "baize tests"
 subprocess.check_call(f"pdm run isort --check --diff {source_dirs}", shell=True)
 subprocess.check_call(f"pdm run black --check --diff {source_dirs}", shell=True)
 subprocess.check_call(f"pdm run flake8 --ignore W503,E203,E501,E731 {source_dirs}", shell=True)
-subprocess.check_call(f"pdm run mypy --ignore-missing-imports {source_dirs}", shell=True)
+subprocess.check_call(f"pdm run mypy --install-types --non-interactive --ignore-missing-imports {source_dirs}", shell=True)
