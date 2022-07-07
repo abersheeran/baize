4b0ac8476949ff5f5b591d8b244ca93077a66e62
abersheeran <me@abersheeran.com>
2022-07-07 05:41:35 +0000

    Optimize run_in_threadpool

--- baize/concurrency.py
+++ baize/concurrency.py
@@ -3,7 +3,7 @@ import functools
 import sys
 from concurrent.futures import ThreadPoolExecutor as _ThreadPoolExecutor
 from contextvars import copy_context
-from typing import Any, Callable, TypeVar
+from typing import Any, Callable, TypeVar, cast
 
 __all__ = [
     "get_running_loop",
@@ -39,16 +39,17 @@ class ThreadPoolExecutor(_ThreadPoolExecutor):  # type: ignore
         )
 
 
-if sys.version_info[:2] < (3, 9):
-    DEFAULT_EXECUTOR = ThreadPoolExecutor()
+async def run_in_threadpool(__fn: Callable[..., T], *args: Any, **kwargs: Any) -> T:
+    """
+    Asynchronously run function fn in a separate thread.
+    Any *args and **kwargs supplied for this function are directly passed
+    to fn. Also, the current :class:`contextvars.Context` is propogated,
+    allowing context variables from the main thread to be accessed in the
+    separate thread.
 
-    async def run_in_threadpool(__fn: Callable[..., T], *args: Any, **kwargs: Any) -> T:
-        loop = get_running_loop()
-        return await loop.run_in_executor(
-            DEFAULT_EXECUTOR, functools.partial(__fn, *args, **kwargs)
-        )
-
-else:
-
-    async def run_in_threadpool(__fn: Callable[..., T], *args: Any, **kwargs: Any) -> T:
-        return await asyncio.to_thread(__fn, *args, **kwargs)
+    https://github.com/python/cpython/blob/0f56263e62ba91d0baae40fb98947a3a98034a73/Lib/asyncio/threads.py
+    """
+    loop = get_running_loop()
+    ctx = copy_context()
+    func_call = functools.partial(ctx.run, __fn, *args, **kwargs)
+    return await loop.run_in_executor(None, cast(Callable[[], T], func_call))
