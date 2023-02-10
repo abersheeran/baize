2acc47c3d47c10affd74469e472f0a01dc3ee3e8
abersheeran <me@abersheeran.com>
2023-02-10 20:36:29 +0800

    Drop Python3.6

--- script/lint.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-source_dirs = "baize tests"
-subprocess.check_call(f"pdm run isort {source_dirs}", shell=True)
-subprocess.check_call(f"pdm run black {source_dirs}", shell=True)
