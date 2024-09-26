3c260b8cfa6dc9c9178a053fc989411f8f7eaecd
Aber <me@abersheeran.com>
2024-09-26 16:31:24 +0800

    Change type hint docs (#70)
    * Change type hint docs
    
    * fix type check
    
    * fix older version python

--- baize/typing.py
+++ baize/typing.py
@@ -52,7 +52,11 @@ Receive = Callable[[], Awaitable[Message]]
 
 Send = Callable[[Message], Awaitable[None]]
 
-ASGIApp = Callable[[Scope, Receive, Send], Awaitable[None]]
+
+class ASGIApp(Protocol):
+    def __call__(self, scope: Scope, receive: Receive, send: Send) -> Awaitable[None]:
+        ...
+
 
 # WSGI: view PEP3333
 Environ = MutableMapping[str, Any]
@@ -66,11 +70,16 @@ class StartResponse(Protocol):
         status: str,
         response_headers: List[Tuple[str, str]],
         exc_info: Optional[ExcInfo] = None,
-    ) -> None:
+    ) -> Any:
         ...
 
 
-WSGIApp = Callable[[Environ, StartResponse], Iterable[bytes]]
+class WSGIApp(Protocol):
+    def __call__(
+        self, environ: Environ, start_response: StartResponse
+    ) -> Iterable[bytes]:
+        ...
+
 
 # Server-sent Event
 # https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events
